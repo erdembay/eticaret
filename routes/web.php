3ff10ed76721c5c9874e995501ced1a10bdd894d
Erdem Bay <erdem.bay@mobilulasim.com>
2025-01-04 03:26:25 +0300

    checkout ve cart sayfası blade e eklendi

--- routes/web.php
+++ routes/web.php
@@ -3,8 +3,11 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\FrontController;
 use App\Http\Controllers\ProductController;
+use App\Http\Controllers\CartController;
 
 Route::get('/', [FrontController::class, 'index']);
 Route::get('/contact', [FrontController::class, 'contact']);
 Route::get('/products', [ProductController::class, 'index']);
 Route::get('/product/{id}', [ProductController::class, 'detail']);
+Route::get('/cart', [CartController::class, 'index']);
+Route::get('/checkout', [CartController::class, 'checkout']);
