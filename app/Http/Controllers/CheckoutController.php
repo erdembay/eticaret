9c913b2993133a6c85e717d4f32e963a659bca62
Erdem Bay <erdem.bay@mobilulasim.com>
2025-01-04 03:27:54 +0300

    checkout controller oluşturuldu

--- app/Http/Controllers/CheckoutController.php
+++ app/Http/Controllers/CheckoutController.php
@@ -4,11 +4,11 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-class CartController extends Controller
+class CheckoutController extends Controller
 {
     //
     public function index()
     {
-        return view('front.cart');
+        return view('front.checkout');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -4,10 +4,11 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\FrontController;
 use App\Http\Controllers\ProductController;
 use App\Http\Controllers\CartController;
+use App\Http\Controllers\CheckoutController;
 
 Route::get('/', [FrontController::class, 'index']);
 Route::get('/contact', [FrontController::class, 'contact']);
 Route::get('/products', [ProductController::class, 'index']);
 Route::get('/product/{id}', [ProductController::class, 'detail']);
 Route::get('/cart', [CartController::class, 'index']);
-Route::get('/checkout', [CartController::class, 'checkout']);
+Route::get('/checkout', [CheckoutController::class, 'index']);
